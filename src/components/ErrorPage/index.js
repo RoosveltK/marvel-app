c27cce4ac3527e58f10d125949d01f66cd43624e
RoosveltK <Roosvelt.kenne@gmail.com>
2021-03-30 07:55:49 +0200

    Error page stylisee

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import imgBatman from "../../images/batman.png";
 
 const ErrorPage = () => {
+  const styleH2 = {
+    textAlign: "center",
+  };
+  const styleTof = {
+    display: "block",
+    margin: "40px auto",
+  };
   return (
     <div className="quiz-bg">
-      <div className="container">Eroorrrrrrrrrr</div>
+      <div className="container">
+        <h2 style={styleH2}>Oups, cette page n'éxiste pas 😫 </h2>
+        <img style={styleTof} src={imgBatman} alt="Image for Error page" />
+      </div>
     </div>
   );
 };
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Footer = () => {
+  const styleText = {
+    textAlign: "center",
+  };
   return (
     <footer>
       <div className="footer-container">
-        <p>Projet réalisé lors de l'apprentissage de React par KN COORP </p>
-        <p>==================================== </p>
+        <p style={styleText}>
+          Projet réalisé lors de l'apprentissage de React js <br />
+          KN COORP PROJECT&trade;
+        </p>
       </div>
     </footer>
   );
