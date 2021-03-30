d10c72e5928a8023d6f5c55c02c08a39f24e0341
RoosveltK <Roosvelt.kenne@gmail.com>
2021-03-30 06:12:41 +0200

    Initialisation du projet

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App.css";
+import Footer from "../Footer";
+import Header from "../Header";
+import Landing from "../Landing";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Landing />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Projet réalisé lors de l'apprentissage de React par KN COORP </p>
+        <p>==================================== </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
