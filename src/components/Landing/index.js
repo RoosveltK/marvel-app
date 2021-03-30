f5dca46c998d74c3cb56ed654c4579e981c71bad
RoosveltK <Roosvelt.kenne@gmail.com>
2021-03-30 06:51:49 +0200

    Animation des griffes de wolverin

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useRef, useEffect, useState, Fragment } from "react";
 
 const Landing = () => {
-  return <main className="welcomePage"></main>;
+  const griffeW = useRef(null);
+  const [btnA, setbtnA] = useState(false);
+
+  useEffect(() => {
+    griffeW.current.classList.add("startingImg");
+    setTimeout(() => {
+      griffeW.current.classList.remove("startingImg");
+      setbtnA(true);
+    }, 1000);
+  }, []);
+
+  const anim1 = btnA && (
+    <Fragment>
+      <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Connexion</button>
+      </div>
+    </Fragment>
+  );
+
+  return (
+    <main ref={griffeW} className="welcomePage">
+      {anim1}
+    </main>
+  );
 };
 
 export default Landing;
