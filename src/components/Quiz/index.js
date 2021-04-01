b6e2e2e595c36c3fa727563930c79277e38e7659
RoosveltK <Roosvelt.kenne@gmail.com>
2021-04-01 05:16:03 +0200

    Progress Bar

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import Levels from "../Levels";
+import ProgressBar from "../ProgressBar";
 
 class Quiz extends Component {
   render() {
-    return <div>Pseudo : {this.props.userData.pseudo}</div>;
+    return (
+      <div>
+        <Levels />
+        <ProgressBar />
+        <h2>Question Quiz</h2>
+        <p className="answerOptions">Question 1</p>
+        <p className="answerOptions">Question 1</p>
+        <p className="answerOptions">Question 1</p>
+        <p className="answerOptions">Question 1</p>
+        <button className="btnSubmit">Valider</button>
+      </div>
+    );
   }
 }
 
